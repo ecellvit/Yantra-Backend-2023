1836bb6463da97a5ed44278dc8b98bfcd4139af4
sai-sreekhar <sai.sreekhar@gmail.com>
2023-05-22 01:45:20 +0530

    feat: added event routes,controller

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.get("/heartbeat", function (req, res) {
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/yantra", require("./routes/yantraTeamRoutes"));
+app.use("/api/events", require("./routes/eventRoutes"));
 
 //all invalid urls handled here
 app.all("*", (req, res, next) => {
