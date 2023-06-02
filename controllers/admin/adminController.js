b2cd640a213c7851bffe55317cab2ecf6af4d48c
sai-sreekhar <sai.sreekhar@gmail.com>
2023-06-02 14:07:18 +0530

    chore: cleaned code

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -2,85 +2,72 @@ const AppError = require("../../utils/appError");
 const catchAsync = require("../../utils/catchAsync");
 const User = require("../../models/userModel");
 const yantraTeamModel = require("../../models/yantraTeamModel");
-const { registerTypes } = require("../../utils/constants");
-const yantraPendingApprovals = require("../../models/yantraPendingApprovalsModel");
-const yantraTeamLeaderApprovalsModel = require("../../models/yantraTeamLeaderPendingApprovalsModel");
+const { registerTypes, eventCodes } = require("../../utils/constants");
 
 exports.getAllCounts = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
-  const t10RegisteredUsers = [];
+  let t10RegisteredUsersCount = 0;
+  let nexusRegisteredUsersCount = 0;
+  let devopsRegisteredUsersCount = 0;
   for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[1] === 1) {
-      t10RegisteredUsers.push(users[i]);
+    if (
+      users[i].registeredEvents &&
+      users[i].registeredEvents[eventCodes.T_10] === 1
+    ) {
+      t10RegisteredUsersCount++;
+    }
+
+    if (
+      users[i].registeredEvents &&
+      users[i].registeredEvents[eventCodes.NEXUS] === 1
+    ) {
+      nexusRegisteredUsersCount++;
+    }
+
+    if (
+      users[i].registeredEvents &&
+      users[i].registeredEvents[eventCodes.DEVOPS] === 1
+    ) {
+      devopsRegisteredUsersCount++;
     }
   }
 
-  const nexusRegisteredUsers = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[2] === 1) {
-      nexusRegisteredUsers.push(users[i]);
-    }
-  }
+  const yantraTeams = await yantraTeamModel.find();
 
-  const devopsRegisteredTeams = [];
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].registeredEvents && users[i].registeredEvents[3] === 1) {
-      devopsRegisteredTeams.push(users[i]);
-    }
-  }
-
-  const yantraTeams = await yantraTeamModel
-    .find(
-      {},
-      {
-        _id: 0,
-        noOfTimesTeamNameChanged: 0,
-        noOfPendingRequests: 0,
-        __v: 0,
-        teamLeaderId: 0,
-      }
-    )
-    .populate("members", {
-      email: 1,
-      firstName: 1,
-      lastName: 1,
-      mobileNumber: 1,
-      regNo: 1,
-      _id: 0,
-    });
-
-  const yantraTeamsWithTwoMembers = [];
-  const yantraTeamsWithThreeMembers = [];
-  const yantraTeamsWithFourMembers = [];
-  let submissionCount = 0;
+  let yantraTeamsWithOneMemberCount = 0;
+  let yantraTeamsWithTwoMembersCount = 0;
+  let yantraTeamsWithThreeMembersCount = 0;
+  let yantraTeamsWithFourMembersCount = 0;
+  let yantraIdeaSubmissionCount = 0;
 
   for (let i = 0; i < yantraTeams.length; i++) {
     if (yantraTeams[i].projectName) {
-      submissionCount++;
+      yantraIdeaSubmissionCount++;
     }
-
-    if (yantraTeams[i].members.length === 2) {
-      yantraTeamsWithTwoMembers.push(yantraTeams[i]);
+    if (yantraTeams[i].members.length === 1) {
+      yantraTeamsWithOneMemberCount++;
+    } else if (yantraTeams[i].members.length === 2) {
+      yantraTeamsWithTwoMembersCount++;
     } else if (yantraTeams[i].members.length === 3) {
-      yantraTeamsWithThreeMembers.push(yantraTeams[i]);
+      yantraTeamsWithThreeMembersCount++;
     } else if (yantraTeams[i].members.length === 4) {
-      yantraTeamsWithFourMembers.push(yantraTeams[i]);
+      yantraTeamsWithFourMembersCount++;
     }
   }
 
   res.status(200).json({
     message: "Data Fetched Successfully",
     Number_Of_Users_LoggedIn: users.length,
-    Number_Of_Users_Registered_for_Devops_Workshop:
-      devopsRegisteredTeams.length,
-    Number_of_Users_Registered_for_T10_Workshop: t10RegisteredUsers.length,
-    Number_of_Users_Registered_for_Nexus_Workshop: nexusRegisteredUsers.length,
+    Number_Of_Users_Registered_for_Devops_Workshop: devopsRegisteredUsersCount,
+    Number_of_Users_Registered_for_T10_Workshop: t10RegisteredUsersCount,
+    Number_of_Users_Registered_for_Nexus_Workshop: nexusRegisteredUsersCount,
     No_of_Ignitia_Hack_Teams: yantraTeams.length,
-    No_of_Ignitia_Hack_Teams_Submitted: submissionCount,
-    No_of_Ignitia_Hack_Teams_With_2_Members: yantraTeamsWithTwoMembers.length,
-    No_of_Ignitia_Hack_Teams_With_3_Members: yantraTeamsWithThreeMembers.length,
-    No_of_Ignitia_Hack_Teams_With_4_Members: yantraTeamsWithFourMembers.length,
+    No_of_Ignitia_Hack_Teams_Submitted: yantraIdeaSubmissionCount,
+    No_of_Ignitia_Hack_Teams_With_1_Member: yantraTeamsWithOneMemberCount,
+    No_of_Ignitia_Hack_Teams_With_2_Members: yantraTeamsWithTwoMembersCount,
+    No_of_Ignitia_Hack_Teams_With_3_Members: yantraTeamsWithThreeMembersCount,
+    No_of_Ignitia_Hack_Teams_With_4_Members: yantraTeamsWithFourMembersCount,
   });
 });
 
